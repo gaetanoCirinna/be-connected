30a764207ac863a586c8ffe987010b87356e6f41
gaetano.cirinna <cirinna.gg@gmail.com>
2020-03-28 17:05:48 +0100

    finita prima versione

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { BreadcrumbNavComponent } from './widget/breadcrumb-nav/breadcrumb-nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ProfilePageComponent,
-    BreadcrumbNavComponent
+    BreadcrumbNavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
