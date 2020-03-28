bc13bc81be5c33c8f53cfaf1c34a6aa7a8fdde1c
gaetano.cirinna <cirinna.gg@gmail.com>
2020-03-28 14:58:34 +0100

    add nav breadcrumb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { BreadcrumbNavComponent } from './widget/breadcrumb-nav/breadcrumb-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProfilePageComponent,
+    BreadcrumbNavComponent
   ],
   imports: [
     BrowserModule,
